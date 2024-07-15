8537e1b278763011d650a201f1361fb1ecbb6297
Iorrah Mota <iorrah.gonzalez@gmail.com>
2024-07-15 14:08:05 -0300

    Add Navigation Bar Structure

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -1,5 +1,16 @@
-const NavigationBar = () => {
-  return <nav>Bookings Manager</nav>;
-};
+import Logo from "./logo.png";
+import User from "./user.png";
+
+const NavigationBar = () => (
+  <nav className="shadow-sm bg-slate-50">
+    <div className="container mx-auto flex justify-between py-4 items-center">
+      <h1>
+        <img src={Logo} alt="Bookings Manageer Logo" className="w-44" />
+      </h1>
+
+      <img src={User} alt="User Profile Image" className="rounded-full w-8" />
+    </div>
+  </nav>
+);
 
 export default NavigationBar;
