0c947708a23a960a58020e28cc50f39696c39976
Iorrah Mota <iorrah.gonzalez@gmail.com>
2024-07-18 14:56:28 -0300

    Improve bookings list visibility

--- src/pages/BookingsPage/BookingsList/index.tsx
+++ src/pages/BookingsPage/BookingsList/index.tsx
@@ -18,49 +18,51 @@ export const BookingsList: BookingsListType = ({
   const ringSyle = "ring-2 ring-blue-600 ring-inset";
 
   return (
-    <div>
-      {bookings.map(booking => {
-        const bookingRingStyle =
-          selectedBookingId === booking.id ? ringSyle : "";
+    <>
+      <div className="max-h-[33.9rem] overflow-scroll mb-8">
+        {bookings.map(booking => {
+          const bookingRingStyle =
+            selectedBookingId === booking.id ? ringSyle : "";
 
-        return (
-          <button
-            className={`text-left block py-4 px-6 w-full mb-4 bg-white rounded-lg shadow-lg ${bookingRingStyle}`}
-            onClick={() => onSelectBooking(booking.id)}
-            key={booking.id}
-          >
-            <div className="flex justify-between items-center">
-              <div>
-                <p className="font-semibold">{booking.guestName}</p>
+          return (
+            <button
+              className={`text-left block py-4 px-6 w-full mb-4 bg-white rounded-lg shadow-lg ${bookingRingStyle}`}
+              onClick={() => onSelectBooking(booking.id)}
+              key={booking.id}
+            >
+              <div className="flex justify-between items-center">
+                <div>
+                  <p className="font-semibold">{booking.guestName}</p>
 
-                <p className="text-sm">
-                  {fullDate(booking.checkIn)} - {fullDate(booking.checkOut)}
-                </p>
+                  <p className="text-sm">
+                    {fullDate(booking.checkIn)} - {fullDate(booking.checkOut)}
+                  </p>
 
-                <p className="text-slate-500 text-sm">
-                  {Number(booking.adultsAmount) +
-                    Number(booking.childrenAmount)}{" "}
-                  guests
-                </p>
+                  <p className="text-slate-500 text-sm">
+                    {Number(booking.adultsAmount) +
+                      Number(booking.childrenAmount)}{" "}
+                    guests
+                  </p>
+                </div>
+
+                <div className="mt-1 flex items-center gap-x-1.5">
+                  {booking.status === 1 ? (
+                    <>
+                      <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                        <div className="h-2 w-2 rounded-full bg-emerald-500"></div>
+                      </div>
+
+                      <p className="text-xs leading-5 text-gray-500">
+                        {formatStatus(booking.status)}
+                      </p>
+                    </>
+                  ) : null}
+                </div>
               </div>
-
-              <div className="mt-1 flex items-center gap-x-1.5">
-                {booking.status === 1 ? (
-                  <>
-                    <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                      <div className="h-2 w-2 rounded-full bg-emerald-500"></div>
-                    </div>
-
-                    <p className="text-xs leading-5 text-gray-500">
-                      {formatStatus(booking.status)}
-                    </p>
-                  </>
-                ) : null}
-              </div>
-            </div>
-          </button>
-        );
-      })}
+            </button>
+          );
+        })}
+      </div>
 
       <button
         className={`bg-slate-100 hover:bg-slate-200 px-5 py-4 text-sm leading-5 rounded-lg font-semibold text-blue-600 border border-slate-300 shadow-sm w-full ${
@@ -70,6 +72,6 @@ export const BookingsList: BookingsListType = ({
       >
         Create Booking
       </button>
-    </div>
+    </>
   );
 };
